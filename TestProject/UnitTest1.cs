d365df4685ddda745f77c98743925b500730f6f9
Paul Jones <15299@global.ul.com>
2023-05-22 08:55:20 +0100

    Added some output

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -1,10 +1,20 @@
+using Xunit.Abstractions;
+
 namespace TestProject
 {
     public class UnitTest1
     {
+        ITestOutputHelper _output;
+
+        public UnitTest1(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
         [Fact]
         public void Test1()
         {
+            _output.WriteLine("Does 1 equal 1....");
             Assert.Equal(1,1);
         }
     }
